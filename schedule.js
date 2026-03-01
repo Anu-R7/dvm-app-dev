67eb80a2595d7668231e1d093a90f25b63cd756e
Anu-R7 <232847570+Anu-R7@users.noreply.github.com>
2026-03-01 22:05:15 +0530

    Create schedule.js

---
+++ schedule.js
@@ -0,0 +1,18 @@
+export const events = [
+  { id: 1, name: "Rocktaves", category: "Music", day: 1, time: "22:00", venue: "Auditorium", registrations: 1125 },
+  { id: 2, name: "Code Relay", category: "Tech", day: 2, time: "10:00", venue: "NAB Audi", registrations: 278 },
+  { id: 3, name: "Robowars", category: "Tech", day: 0, time: "21:00", venue: "Rotunda", registrations: 820 },
+
+  { id: 4, name: "Street Dance Faceoff", category: "Cultural", day: 3, time: "18:00", venue: "SAC Stage", registrations: 640 },
+  { id: 5, name: "Hackathon 24H", category: "Tech", day: 1, time: "09:00", venue: "Library Lawns", registrations: 450 },
+  { id: 6, name: "Music Nite", category: "Music", day: 3, time: "20:30", venue: "Auditorium", registrations: 1178 },
+  { id: 7, name: "Sherlock v/s Moriarty", category: "Fun", day: 1, time: "14:00", venue: "Campus Grounds", registrations: 720 },
+  { id: 8, name: "Stand-up Comedy Nite", category: "Comedy", day: 2, time: "19:00", venue: "NAB Audi", registrations: 530 },
+  { id: 9, name: "Valorant Tournament", category: "Tech", day: 0, time: "23:00", venue: "CRISS Event Stall", registrations: 390 },
+  { id: 10, name: "Fashion Fiesta", category: "Cultural", day: 2, time: "21:30", venue: "Rotunda", registrations: 370 },
+  { id: 11, name: "Period Pain Simulator", category: "Challenges", day: 1, time: "16:00", venue: "Beside M-Lawns", registrations: 310 },
+  { id: 12, name: "CrAC Face Painting", category: "Arts", day: 3, time: "11:00", venue: "CrAC Stall", registrations: 150 },
+  { id: 13, name: "RC Car Racing", category: "Games", day: 1, time: "17:30", venue: "RC Club Stall", registrations: 220 },
+  { id: 14, name: "DJ Nite", category: "Music", day: 2, time: "23:30", venue: "T-Lawns", registrations: 670 },
+  { id: 15, name: "Prom Nite", category: "Cultural", day: 4, time: "22:00", venue: "SAC Hall", registrations: 265 }
+];
